c9920253830a1ef03272fcde537aba88f8be5067
Kim-SiHwan <tjdans4166@naver.com>
2021-05-12 22:41:33 +0900

    #2 Redis Config

--- src/main/java/kim/sihwan/daangn/DaangnApplication.java
+++ src/main/java/kim/sihwan/daangn/DaangnApplication.java
@@ -2,7 +2,9 @@ package kim.sihwan.daangn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class DaangnApplication {
 
