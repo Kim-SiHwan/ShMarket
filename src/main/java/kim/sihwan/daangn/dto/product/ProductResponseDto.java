624b8e4e2c2d6c69213c3549ec591ebe2d75619f
Kim-SiHwan <tjdans4166@naver.com>
2021-05-13 00:48:44 +0900

    #12 상품, 태그의 DTO 추가

---
+++ src/main/java/kim/sihwan/daangn/dto/product/ProductResponseDto.java
@@ -0,0 +1,49 @@
+package kim.sihwan.daangn.dto.product;
+
+import kim.sihwan.daangn.domain.product.Product;
+import kim.sihwan.daangn.dto.product.productAlbum.ProductAlbumResponseDto;
+import kim.sihwan.daangn.dto.tag.TagResponseDto;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class ProductResponseDto {
+    private final Long id;
+    private final String area;
+    private final String title;
+    private final String content;
+    private final String username;
+    private final String nickname;
+    private final String category;
+    private final String createDate;
+    private final int readCount;
+    private final List<ProductAlbumResponseDto> productAlbums;
+    private final List<TagResponseDto> tags;
+
+    public static ProductResponseDto toDto(Product product){
+        return ProductResponseDto
+                .builder()
+                .id(product.getId())
+                .area(product.getArea())
+                .username(product.getMember().getUsername())
+                .nickname(product.getMember().getNickname())
+                .title(product.getTitle())
+                .content(product.getContent())
+                .category(product.getCategory())
+                .createDate(product.getCreateDate().toString())
+                .productAlbums(product.getProductAlbums()
+                        .stream()
+                        .map(ProductAlbumResponseDto::new)
+                        .collect(Collectors.toList())
+                )
+                .tags(product.getProductTags()
+                        .stream()
+                        .map(tag-> new TagResponseDto(tag.getTag()))
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
