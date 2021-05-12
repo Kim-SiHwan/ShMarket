c81a928e697bc5b50735528b53be7effa3e73093
Kim-SiHwan <tjdans4166@naver.com>
2021-05-12 22:41:45 +0900

    #2 Application Runner Config

---
+++ src/main/java/kim/sihwan/daangn/config/AppRunnerConfig.java
@@ -0,0 +1,17 @@
+
+package kim.sihwan.daangn.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+class AppRunnerConfig implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
