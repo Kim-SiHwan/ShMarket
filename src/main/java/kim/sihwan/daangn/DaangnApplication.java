8cfec858072c85b21f16e96781a8242aa53fb959
Kim-SiHwan <tjdans4166@naver.com>
2021-05-12 22:32:51 +0900

    #1 프로젝트 생성

---
+++ src/main/java/kim/sihwan/daangn/DaangnApplication.java
@@ -0,0 +1,13 @@
+package kim.sihwan.daangn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaangnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kim/sihwan/daangn/DaangnApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.sihwan.daangn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
