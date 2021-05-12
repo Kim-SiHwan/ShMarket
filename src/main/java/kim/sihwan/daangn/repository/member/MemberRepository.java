17867600aba87f11f7d2549d0f226472fb330ff5
Kim-SiHwan <tjdans4166@naver.com>
2021-05-12 23:40:43 +0900

    #8 MemberRepository 메소드 추가

--- src/main/java/kim/sihwan/daangn/repository/member/MemberRepository.java
+++ src/main/java/kim/sihwan/daangn/repository/member/MemberRepository.java
@@ -4,6 +4,7 @@ import kim.sihwan.daangn.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
-
+    Member findMemberByUsername(String username);
+    Member findMemberByNickname(String nickname);
 
 }
