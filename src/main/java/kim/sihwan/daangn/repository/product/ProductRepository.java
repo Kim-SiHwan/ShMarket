fbf60471770cd3acb5d5927c70d7efd385dc49ab
Kim-SiHwan <tjdans4166@naver.com>
2021-05-14 17:12:22 +0900

    #12 Product Repository의 findAll, findById를 Override 해 Entity Graph를 통해 조회 시 주변 엔티티 로딩으로 변경함으로 쿼리 줄이기

--- src/main/java/kim/sihwan/daangn/repository/product/ProductRepository.java
+++ src/main/java/kim/sihwan/daangn/repository/product/ProductRepository.java
@@ -1,8 +1,19 @@
 package kim.sihwan.daangn.repository.product;
 
 import kim.sihwan.daangn.domain.product.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
+    @Override
+    @EntityGraph(attributePaths = {"member","productTags","productTags.tag","productAlbums","productInteresteds"})
+    List<Product> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {"member","productTags","productTags.tag","productAlbums","productInteresteds"})
+    Optional<Product> findById(Long id);
 }
