b48fb586e3124db60112cdbd2c71d0d847b8ee5f
Kim-SiHwan <tjdans4166@naver.com>
2021-05-27 23:35:38 +0900

    #32 채팅을 구현하기 위한 Web Socket 설정

---
+++ src/main/java/kim/sihwan/daangn/config/webSocket/WebSocketConfig.java
@@ -0,0 +1,41 @@
+package kim.sihwan.daangn.config.webSocket;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry
+                .addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        registry
+                .setApplicationDestinationPrefixes("/app")
+                .enableStompBrokerRelay("/topic")
+                .setRelayPort(61613)
+                .setClientLogin(username)
+                .setClientPasscode(password);
+//                .enableSimpleBroker("/send")
+
+        
+    }
+
+}
+
