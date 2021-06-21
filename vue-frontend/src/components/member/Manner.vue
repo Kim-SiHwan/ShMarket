7bce9493836130f173e690bd1359a28ba8df5227
Kim-SiHwan <tjdans4166@naver.com>
2021-06-21 18:54:31 +0900

    #33 프로필 하위 컴포넌트 코드정리

--- vue-frontend/src/components/member/Manner.vue
+++ vue-frontend/src/components/member/Manner.vue
@@ -1,61 +1,60 @@
 <template>
   <v-container>
-    <v-icon color="orange" size="50">mdi-emoticon-happy-outline</v-icon>
+    <v-icon color = "orange" size = "50">mdi-emoticon-happy-outline</v-icon>
     <strong>{{ paramNickname }}</strong>님의 매너평가
     <v-data-table
-        v-if="manners"
-        id="mannerTable"
-        :items="manners"
-        :items-per-page="itemsPerPage"
-        :page.sync="page"
-        class="elevation-1 mt-3"
+        id = "mannerTable"
+        :items = "manners"
+        :items-per-page = "itemsPerPage"
+        :page.sync = "page"
+        class = "elevation-1 mt-3"
         hide-default-footer
         hide-default-header
-        no-data-text="첫 평가를 작성해보세요!"
-        style="border: solid green 1px"
-        @page-count="pageCount= $event">
+        no-data-text = "첫 평가를 작성해보세요!"
+        style = "border: solid green 1px"
+        @page-count = "pageCount= $event">
 
-      <template v-slot:item="manners">
-        <tr align="left">
+      <template v-slot:item = "manners">
+        <tr align = "left">
           <td>
-          <div>
-            <p class="mt-4">{{ manners.item.content }}</p>
-          </div>
+            <div>
+              <p class = "mt-4">{{ manners.item.content }}</p>
+            </div>
           </td>
         </tr>
       </template>
     </v-data-table>
 
     <v-pagination
-        v-model="page"
-        :length="pageCount">
+        v-model = "page"
+        :length = "pageCount">
 
     </v-pagination>
 
-    <v-row justify="center">
-      <v-dialog v-model="dialog" max-width="450" persistent>
+    <v-row justify = "center">
+      <v-dialog v-model = "dialog" max-width = "450" persistent>
         <v-card>
-          <v-card-title class="headline"><small>매너평가</small></v-card-title>
+          <v-card-title class = "headline"><small>매너평가</small></v-card-title>
           <v-select
-              v-model="selectedType"
-              :items="typeItems"
-              class="mr-2 ml-2"
+              v-model = "selectedType"
+              :items = "typeItems"
+              class = "mr-2 ml-2"
               outlined>
 
           </v-select>
           <v-select
-              v-if="selectedType === '매너 평가'"
-              v-model="selectedManner"
-              :items="mannerItems"
-              class="mr-2 ml-2"
+              v-if = "selectedType === '매너 평가'"
+              v-model = "selectedManner"
+              :items = "mannerItems"
+              class = "mr-2 ml-2"
               outlined>
 
           </v-select>
           <v-select
-              v-else-if="selectedType === '비매너 평가'"
-              v-model="selectedManner"
-              :items="nonMannerItems"
-              class="mr-2 ml-2"
+              v-else-if = "selectedType === '비매너 평가'"
+              v-model = "selectedManner"
+              :items = "nonMannerItems"
+              class = "mr-2 ml-2"
               outlined>
 
           </v-select>
@@ -63,12 +62,12 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn
-                color="primary"
-                @click="addManner">평가하기
+                color = "primary"
+                @click = "addManner">평가하기
             </v-btn>
             <v-btn
-                color="error"
-                @click="dialog = false">취소하기
+                color = "error"
+                @click = "dialog = false">취소하기
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -77,15 +76,15 @@
 
     <v-fab-transition>
       <v-btn
-          v-if="paramNickname !== nickname"
-          color="orange"
+          v-if = "paramNickname !== nickname"
+          color = "orange"
           dark
           fab
           fixed
           right
           small
-          style="bottom: 80px;"
-          @click="dialog=true">
+          style = "bottom: 80px;"
+          @click = "dialog=true">
         <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-fab-transition>
@@ -95,21 +94,21 @@
 
 <script>
 export default {
-  name: "Manner",
+  name    : "Manner",
   data() {
     return {
-      dialog: '',
-      paramNickname: '',
+      dialog        : '',
+      paramNickname : '',
       selectedManner: '',
-      selectedType: '',
-      itemsPerPage: 8,
-      pageCount: 0,
-      page: 1,
-      typeItems: [
+      selectedType  : '',
+      itemsPerPage  : 8,
+      pageCount     : 0,
+      page          : 1,
+      typeItems     : [
         '매너 평가',
         '비매너 평가'
       ],
-      mannerItems: [
+      mannerItems   : [
         '친절해요.',
         '매너가 좋아요.',
         '연락이 빨라요',
@@ -127,12 +126,12 @@ export default {
       ],
     }
   },
-  methods: {
+  methods : {
     addManner() {
       let data = {
         nickname: this.paramNickname,
-        type: this.selectedType,
-        content: this.selectedManner
+        type    : this.selectedType,
+        content : this.selectedManner
       }
       this.$store.dispatch('REQUEST_ADD_MANNER', data);
       this.selectedType = '';
--- vue-frontend/src/components/member/MyLikeProduct.vue
+++ vue-frontend/src/components/member/MyLikeProduct.vue
@@ -1,113 +1,120 @@
 <template>
-  <div id="myLikeProductListDiv" class="row justify-center mt-15">
-    <ul v-for="(list,index) in productList" :key="index"
-        style="list-style: none">
-      <li id="myLikeListDiv">
-        <div class="p-5 mb-5 rounded float-left"
-             style="width: 300px; height: 400px; border: 2px solid green">
-          <div class="card-body">
-            <span class="mt-3" style="margin-left: 130px"><strong>{{ list.title }}</strong></span>
-            <br>
-            <span class="float-left card-subtitle">
-              <router-link
-                  :to="{path:'/profile',query:{nickname:list.nickname}}"
-                  style="text-decoration: none; color:black">
-                <span class="float-left ml-3 mt-1 mr-3"><small>작성자 : {{ list.nickname }}</small></span></router-link>
-              <br>
-              <span class="float-left ml-3 mt-1 mr-3"> <small>작성일 : {{ displayedAt(list.createDate) }}</small></span>
-              <br>
-              <span class="float-left ml-3 mt-1 mr-3"><small>지역 : {{ list.area }}</small></span>
-              </span>
-            <br>
+  <v-container>
 
-            <div id="myLikeProductListImgDiv" style="height: 100%; width: 100%">
+    <v-icon color = "pink" size = "50">mdi-heart</v-icon>
+    <strong>{{ nickname }}</strong>님의 관심 상품
+    <p><small v-if = "productList">{{ productList.length }}개의 관심 상품이 있습니다</small></p>
+
+    <div id = "myLikeProductListDiv" class = "row justify-center mt-15">
+      <ul v-for = "(list,index) in productList" :key = "index"
+          style = "list-style: none">
+        <li id = "myLikeListDiv">
+          <div class = "p-5 mb-5 rounded float-left"
+               style = "width: 300px; height: 400px; border: 2px solid green">
+            <div class = "card-body">
+              <span class = "mt-3" style = "margin-left: 130px"><strong>{{ list.title }}</strong></span>
+              <br>
+              <span class = "float-left card-subtitle">
               <router-link
-                  :to="{
+                  :to = "{path:'/profile',query:{nickname:list.nickname}}"
+                  style = "text-decoration: none; color:black">
+                <span class = "float-left ml-3 mt-1 mr-3"><small>작성자 : {{ list.nickname }}</small></span></router-link>
+              <br>
+              <span class = "float-left ml-3 mt-1 mr-3"> <small>작성일 : {{ displayedAt(list.createDate) }}</small></span>
+              <br>
+              <span class = "float-left ml-3 mt-1 mr-3"><small>지역 : {{ list.area }}</small></span>
+              </span>
+              <br>
+
+              <div id = "myLikeProductListImgDiv" style = "height: 100%; width: 100%">
+                <router-link
+                    :to = "{
                     path:'/productDetail',
                     query:{productId:list.id}
                   }">
-                <v-img
-                    :src="list.thumbnail"
-                    class="mt-15 mr-3 ml-13 grey lighten-3"
-                    height="200"
-                    width="200">
+                  <v-img
+                      :src = "list.thumbnail"
+                      class = "mt-15 mr-3 ml-13 grey lighten-3"
+                      height = "200"
+                      width = "200">
 
-                </v-img>
-              </router-link>
-              <div
-                  v-if="list.tags.length ===0 "
-                  style="height: 34px">
-              </div>
+                  </v-img>
+                </router-link>
+                <div
+                    v-if = "list.tags.length ===0 "
+                    style = "height: 34px">
+                </div>
 
-              <div v-else id="myLikeProductTagDiv" class="mt-5">
-                <v-row align-content="center" justify="center">
-                  <div v-for="(tags,index) in list.tags" :key="index" style="list-style: none; display: inline">
-                    <v-chip
-                        class="ml-0 mr-1 pr-2 pl-2"
-                        color="info"
-                        label
-                        small>
-                      {{ tags.tag }}
-                    </v-chip>
-                  </div>
-                </v-row>
-              </div>
+                <div v-else id = "myLikeProductTagDiv" class = "mt-5">
+                  <v-row align-content = "center" justify = "center">
+                    <div v-for = "(tags,index) in list.tags" :key = "index" style = "list-style: none; display: inline">
+                      <v-chip
+                          class = "ml-0 mr-1 pr-2 pl-2"
+                          color = "info"
+                          label
+                          small>
+                        {{ tags.tag }}
+                      </v-chip>
+                    </div>
+                  </v-row>
+                </div>
 
-              <div id="myLikeProductListIconDiv" class="mt-8">
-                <v-row align-content="center" justify="center">
-                  <v-icon
-                      color="blue darken-4">
-                    mdi-message-text
-                  </v-icon>
-                  <span class="mt-1">{{ list.readCount }}</span>
+                <div id = "myLikeProductListIconDiv" class = "mt-8">
+                  <v-row align-content = "center" justify = "center">
+                    <v-icon
+                        color = "blue darken-4">
+                      mdi-message-text
+                    </v-icon>
+                    <span class = "mt-1">{{ list.readCount }}</span>
 
-                  <v-icon
-                      color="green">
-                    mdi-image-multiple
-                  </v-icon>
-                  <span class="mt-1">{{ list.productAlbumCount }}</span>
+                    <v-icon
+                        color = "green">
+                      mdi-image-multiple
+                    </v-icon>
+                    <span class = "mt-1">{{ list.productAlbumCount }}</span>
 
-                  <v-btn
-                      v-if="list.like"
-                      color="pink"
-                      dark
-                      icon
-                      @click="pushLike(list.id)">
+                    <v-btn
+                        v-if = "list.like"
+                        color = "pink"
+                        dark
+                        icon
+                        @click = "pushLike(list.id)">
 
-                    <v-icon dark>mdi-heart</v-icon>
+                      <v-icon dark>mdi-heart</v-icon>
 
-                  </v-btn>
+                    </v-btn>
 
-                  <v-btn
-                      v-else
-                      color="grey"
-                      dark
-                      icon
-                      @click="pushLike(list.id)">
+                    <v-btn
+                        v-else
+                        color = "grey"
+                        dark
+                        icon
+                        @click = "pushLike(list.id)">
 
-                    <v-icon dark>mdi-heart</v-icon>
+                      <v-icon dark>mdi-heart</v-icon>
 
-                  </v-btn>
-                </v-row>
+                    </v-btn>
+                  </v-row>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </li>
-    </ul>
-  </div>
+        </li>
+      </ul>
+    </div>
+  </v-container>
 
 </template>
 
 <script>
 export default {
-  name: "MyLikeProduct",
+  name    : "MyLikeProduct",
   data() {
     return {
-      paramNickname:''
+      paramNickname: ''
     }
   },
-  methods: {
+  methods : {
     displayedAt(createdAt) {
       createdAt = new Date(createdAt);
       const milliSeconds = new Date() - createdAt
@@ -155,7 +162,7 @@ export default {
     this.paramNickname = this.$route.query.nickname;
   },
   mounted() {
-    this.$store.dispatch('REQUEST_GET_MY_LIKE_PRODUCT', this.nickname);
+    this.$store.dispatch('REQUEST_GET_MY_LIKE_PRODUCT', this.paramNickname);
   }
 }
 </script>
